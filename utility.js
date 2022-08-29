9e4456dc1cfb4ab544223b927682dd7b93377fc4
prime <prime@adsl-172-10-2-46.dsl.sndg02.sbcglobal.net>
2022-08-29 14:53:44 +0700

    first commit

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken= "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MTkxMDI0LCJleHAiOjE2NTkxOTQ2MjQsIm5iZiI6MTY1OTE5MTAyNCwianRpIjoiSHZLbzVQS0JMVkFrbmw5ZSIsInN1YiI6ODEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.CevJ3ODGj98buZHpYmYp75xrLMXBNCJ1WNB6EnMYph8" ; // put access token
+const accessToken= "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzU4ODMxLCJleHAiOjE2NjE3NjI0MzEsIm5iZiI6MTY2MTc1ODgzMSwianRpIjoiQ2JCU3k1QVJVU2REQkhJWiIsInN1YiI6ODEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.p0XfLxJRsC5mtVTCDieQEQyS_nlqM-n7du0k7zMivX4" ; // put access token
 const student = {
     student_id: 63360682,
     name: '', 
